deafefc301f07db76797e3f517e0bd4887f74396
dpiskovic21 <dpiskovic21@foi.hr>
2024-07-20 15:01:20 +0200

    Dodani dekoratori na dto klasu

--- src/korisnik/dto/create-korisnik.dto.ts
+++ src/korisnik/dto/create-korisnik.dto.ts
@@ -1,7 +1,18 @@
+import { IsBoolean, IsString } from 'class-validator';
+
 export class CreateKorisnikDTO {
+  @IsString()
   ime: string;
+
+  @IsString()
   prezime: string;
+
+  @IsString()
   email: string;
+
+  @IsString()
   lozinka: string;
+
+  @IsBoolean()
   admin: boolean;
 }
