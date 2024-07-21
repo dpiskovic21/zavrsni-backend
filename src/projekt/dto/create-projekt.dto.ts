4c2c50d3c96e57e860fc95b241034412e097aaea
dpiskovic21 <dpiskovic21@foi.hr>
2024-07-21 12:32:25 +0200

    Dodani dekoratori za validaciju DTO klase

--- src/projekt/dto/create-projekt.dto.ts
+++ src/projekt/dto/create-projekt.dto.ts
@@ -1,1 +1,21 @@
-export class CreateProjektDTO {}
+import {
+  ArrayNotEmpty,
+  ArrayUnique,
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsString,
+} from 'class-validator';
+
+export class CreateProjektDTO {
+  @IsString()
+  @IsNotEmpty()
+  naziv: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @ArrayUnique()
+  @IsInt({ each: true })
+  voditelji: number[];
+}
