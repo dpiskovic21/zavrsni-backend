07d1cc7ecf51f64ee62af54a56ffa06255e94983
dpiskovic21 <dpiskovic21@foi.hr>
2024-07-21 13:36:17 +0200

    Definiran DTO objekt za izradu zadatka

--- src/zadatak/dto/create-zadatak.dto.ts
+++ src/zadatak/dto/create-zadatak.dto.ts
@@ -1,1 +1,40 @@
-export class CreateZadatakDTO {}
+import {
+  IsDate,
+  IsDateString,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+import { Prioritet } from '@prisma/client';
+
+export class CreateZadatakDTO {
+  @IsString()
+  @IsNotEmpty()
+  naziv: string;
+
+  @IsString()
+  @IsNotEmpty()
+  opis: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  projektId: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  izvrsiteljId: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  izvjestiteljId: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEnum(Prioritet)
+  proritet: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  rok: Date;
+}
